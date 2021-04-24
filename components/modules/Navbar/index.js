9d21ebc62160964a6b1c3ceba0cef8ed9ff8d4c8
LuisGonzalez321 <Luis.Gonzalez753@std.uni.edu.ni>
2021-04-23 22:41:11 -0600

    agregando navbar

---
+++ components/modules/Navbar/index.js
@@ -0,0 +1,38 @@
+const index = () => {
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="navbar-brand">
+          <a className="navbar-item">
+            <img
+              src="https://bulma.io/images/bulma-type-white.png"
+              alt="Logo"
+            />
+          </a>
+          <span className="navbar-burger" data-target="navbarMenuHeroA">
+            <span />
+            <span />
+            <span />
+          </span>
+        </div>
+        <div id="navbarMenuHeroA" className="navbar-menu">
+          <div className="navbar-end">
+            <a className="navbar-item is-active">Home</a>
+            <a className="navbar-item">Examples</a>
+            <a className="navbar-item">Documentation</a>
+            <span className="navbar-item">
+              <a className="button is-primary is-inverted">
+                <span className="icon">
+                  <i className="fab fa-github" />
+                </span>
+                <span>Download</span>
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default index;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,65 @@
-import Link from 'next/link'
+import Head from "next/head";
+import Link from "next/link";
+
+
+//Components
+import Navbar from '../components/modules/Navbar';
 
 export default function IndexPage() {
   return (
     <div>
-      Hello World.{' '}
-      <Link href="/about">
-        <a>About</a>
-      </Link>
+      <Head>
+        <title>Portafolio - Luis Gabriel Gonzalez Lopez</title>
+        <meta name="description" content="" />
+      </Head>
+      <main>
+        <Main1 />
+      </main>
     </div>
-  )
+  );
 }
+
+const Main1 = () => {
+  return (
+    <section className="hero is-primary is-medium">
+      {/* Hero head: will stick at the top */}
+      <div className="hero-head">
+        <Navbar />
+      </div>
+      {/* Hero content: will be in the middle */}
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          <p className="title">Title</p>
+          <p className="subtitle">Subtitle</p>
+        </div>
+      </div>
+      {/* Hero footer: will stick at the bottom */}
+      <div className="hero-foot">
+        <nav className="tabs">
+          <div className="container">
+            <ul>
+              <li className="is-active">
+                <a>Overview</a>
+              </li>
+              <li>
+                <a>Modifiers</a>
+              </li>
+              <li>
+                <a>Grid</a>
+              </li>
+              <li>
+                <a>Elements</a>
+              </li>
+              <li>
+                <a>Components</a>
+              </li>
+              <li>
+                <a>Layout</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </section>
+  );
+};
